2d102cc9295593b1cd7af300b78362eb49bc1478
moka-a <aud1220a@gmail.com>
2016-06-25 13:52:56 +0900

    issue #7 foreign key bug fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,8 @@
 class Article < ActiveRecord::Base
-  belongs_to :users
+  belongs_to :users, foreign_key: "user_id"
 
   has_many :article_tags
   has_many :tags, through: :article_tags
 
   has_many :attachments
-
-  foreign_key: "user_id"
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,5 @@
 class Comment < ActiveRecord::Base
-  belongs_to :users
-
-  foreign_key: "user_id"
-  foreign_key: "article_id"
-  foreign_key: "parent_id"
+  belongs_to :users, foreign_key: "user_id"
+  belongs_to :comments, foreign_key: "parent_id"
+  belongs_to :articles, foreign_key: "article_id"
 end
