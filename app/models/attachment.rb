7e1db84aaf12b81d455c4206bb08bdb525f2065c
drunkencoding <drunkencoding@gmail.com>
2016-07-10 13:49:39 +0900

    add validates to attachment model

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,3 +1,15 @@
 class Attachment < ActiveRecord::Base
   belongs_to :articles
+
+  attr_accessor :article_id, :filename, :bytes, :mime
+  
+  validates :article_id, presence: true,
+    numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
+
+  validates :filename, presence: true,
+    format: { without: /\s/ }
+
+  validates :bytes, presence: true,
+    numericality: { only_integer: true, :greater_than_or_equal_to => 0 }
+
 end
--- test/models/attachment_test.rb
+++ test/models/attachment_test.rb
@@ -1,7 +1,43 @@
 require 'test_helper'
 
 class AttachmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @attachment = Attachment.new(article_id: '1', filename: 'filename', bytes: '1')
+  end
+
+  test 'should be valid' do
+    assert @attachment.valid?
+  end
+
+  test 'article_id should be integer' do
+    invalids = %w[integer integer0]
+    invalids.each do |invalid|
+      @attachment.article_id = invalid
+      assert_not @attachment.valid?
+    end
+  end
+  
+  test 'article_id should be grater than or equal to  0' do
+    @attachment.article_id = '-1'
+    assert_not @attachment.valid?
+  end
+
+  test 'filename should be no whitespace' do
+    @attachment.filename = 'white space'
+    assert_not @attachment.valid?
+  end
+  
+  test 'bytes should be integer' do
+    invalids = %w[integer integer0]
+    invalids.each do |invalid|
+      @attachment.bytes = invalid
+      assert_not @attachment.valid?
+    end    
+  end
+
+  test 'bytes should be grater than or equal to 0' do
+    @attachment.bytes = '-1'
+    assert_not @attachment.valid?
+  end    
+
 end
