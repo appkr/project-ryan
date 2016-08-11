aa142b82212b7ca8e7c41e28bf1d091a43a4ff2a
bbigbros <steveim1201@gmail.com>
2016-08-11 13:54:40 +0900

    modified-crud-error

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
     @articles = Article.all
+    @current_user = current_user
   end
 
   def new
@@ -12,11 +13,10 @@ class ArticlesController < ApplicationController
     article.user_id = current_user.id
     respond_to do |format|
       if article.save
-        flash[:alert] = "User was successfully created. Please log in."
-        format.html { redirect_to articles_path, notice: 'User was successfully created.' }
+        format.html { redirect_to articles_path, notice: '글이 성공적으로 작성되었습니다.' }
         format.json { render :show, status: :created, location: article }
       else
-        format.html { render :new }
+        format.html { redirect_to :back, notice: article.errors[:title].first}
         format.json { render json: article.errors, status: :unprocessable_entity }
       end
     end
@@ -24,31 +24,40 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @current_user = current_user
   end
 
   def edit
     @article = Article.find(params[:id])
+    unless @article.user_id == current_user.id
+      redirect_to :back
+    end
   end
 
   def update
     @article = Article.find(params[:id])
     respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to articles_path, notice: 'Post was successfully updated.' }
+        format.html { redirect_to article_path, notice: '글이 성공적으로 수정 되었습니다.' }
         format.json { render :show, status: :ok, location: @article }
       else
-        format.html { render :edit }
+        format.html { redirect_to :back, notice: @article.error[:title].first }
         format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_path }
-      format.json { head :no_content }
+    article = Article.find(params[:id])
+    if article.user_id == current_user.id
+      respond_to do |format|
+        if article.destroy
+          format.html { redirect_to articles_path, notice: '글이 성공적으로 삭제 되었습니다.' }
+        end
+      end
+    else
+      flash[:alert] = "삭제 권한이 없습니다."
+      redirect_to :back
     end
   end
 
